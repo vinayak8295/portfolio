f3f2341de0ca746ba90ba59fe38443a15720d8d6
vinayakchachra <59107652+vinayak8295@users.noreply.github.com>
2023-01-16 17:15:04 +0530

    c

--- js/script.js
+++ js/script.js
@@ -42,3 +42,37 @@ function mode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+const myTags = [
+  'JavaScript', 'CSS', 'HTML',
+  'C', 'C++', 'React',
+  'Python', 'Java', 'git',
+  'django', 'Node.js', 'OpenCV',
+  'GCP', 'MySQL', 'jQuery',
+];
+
+// var tagCloud = TagCloud('.content', myTags);
+
+var tagCloud = TagCloud('.content', myTags,{
+
+  // radius in px
+  radius: 300,
+
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'fast',
+  initSpeed: 'fast',
+
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+
+  // interact with cursor move on mouse out
+  keep: true
+
+}); 
+
+var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
